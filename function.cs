4b3455409068236a21219a1fc0e9ca569696ea17
Mehmet Uğur GÜRAL <delphoiq@gmail.com>
2018-11-26 01:40:53 +0200

    function file updated

--- function.cs
+++ function.cs
@@ -1,42 +1,43 @@
 #r "Newtonsoft.Json"
-using System;
+
+using System.Net;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Net.Http.Headers;
 using System.Collections.Generic;
 
-public static async Task<HttpResponseMessage> Run(HttpRequestMessage req, TraceWriter log)
+public static async Task<HttpResponseMessage> Run(HttpRequestMessage req, ILogger log)
 {
     var statusMsg = "@channel \n";
-    var errorSenderWebSite = req.GetQueryNameValuePairs()
-    .FirstOrDefault(x => string.Compare(x.Key, "website", true) == 0).Value;
     
     //Push message through Slack
     using (var client = new HttpClient()) 
     { 
         client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-        client.DefaultRequestHeaders.Add("User-Agent", "AzureFunctions");
-        
+        client.DefaultRequestHeaders.Add("User-Agent", "AzureFunctions");  
 
         List<Attachment> attachmentList = new List<Attachment>();
         Attachment attachment = new Attachment()
         {
             color = "#ff0000", 
-            text = String.Format("{0} has gone offline !", errorSenderWebSite), 
+            text = String.Format("404 code occurred in your web app !"), 
             ts = (DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0)).TotalSeconds.ToString(),
-            fallback = "Website has gone offline !"
+            fallback = "404 code occurred in your web app !"
         };
 
         attachmentList.Add(attachment);
 
-        var uri = "slack web hook url";
+        var uri = "slackurl goes here";
         var msg = new SlackHook {text = statusMsg, parse = "full", attachments = attachmentList };
         
         StringContent SlackMsg = new StringContent(JsonConvert.SerializeObject(msg));
         HttpResponseMessage response = client.PostAsync(uri, SlackMsg).Result; 
         var responseString = response.Content.ReadAsStringAsync().Result;
 
-        return req.CreateResponse(responseString);
+        return response;
     }
 }
 
